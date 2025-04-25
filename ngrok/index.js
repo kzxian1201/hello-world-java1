43b87ff50329045375dd2b3d81dfcecc192f991c
kzxian1201 <kzxian76@gmail.com>
2025-04-25 12:36:36 +0800

    nnn

--- ngrok/index.js
+++ ngrok/index.js
@@ -1,4 +1,4 @@
-// before run ngrok on node, run the following commands:-nnnnnnn
+// before run ngrok on node, run the following commands:-
 //set NGROK_AUTHTOKEN=2kovYxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 //node index.js
 
